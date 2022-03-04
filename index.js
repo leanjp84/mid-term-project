69e571bb300875f2e1a74959c67d0c92babe3e97
leanjp84 <lean84@gmail.com>
2022-03-04 16:41:54 +0100

    first settings done

---
+++ index.js
@@ -0,0 +1,10 @@
+
+//Prueba de llamada a la API
+async function getWithFetch() {
+    const res = await fetch("http://localhost:3000/posts");
+    const finalRes = await res.json();
+    console.log(finalRes);
+}
+getWithFetch();
+
+//json-server --watch db.json --port 3000
