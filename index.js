3388fe921ab3cfeebb15ec684cba04353fcc3941
leanjp84 <lean84@gmail.com>
2022-03-11 10:11:06 +0100

    Done

--- index.js
+++ index.js
@@ -33,40 +33,6 @@ const renderPosts = async () => {
 }
 window.addEventListener('DOMContentLoaded', () => renderPosts());
 
-/////////////////////////////////// PROJECTS PAGE CARDS
-
-const container2 = document.querySelector('.container-projects2');
-
-const renderPosts2 = async () => {
-    let uri2 = "https://marketplace.freelancewebdesign.online/wp-json/wp/v2/projects";
-
-    const res2 = await fetch(uri2);
-    const posts2 = await res2.json();
-    //console.log(posts2);
-
-    let template2 = " ";
-    posts2.forEach(post => {
-        template2 += `
-        <div class="project-card project-img">
-            <img src="${post.better_featured_image.source_url}">
-            <div class="carta1">
-                 <div class="project-text">
-                        <p id="project-title1" class="body-medium">${post.title.rendered}</p>
-                        <p id="description1 " class="headline-regular" ">${post.acf.description}</p>
-                        <br><br>
-                        <a class=" headline-regular" href="">Learn More</a>
-                    </div>
-            </div>
-         </div>
-        `
-    });
-    container2.innerHTML = template2;
-}
-
-window.addEventListener('DOMContentLoaded', () => renderPosts2());
-
-
-//  json-server --watch db.json --port 3000
 
 ///////////////////////////////////////// FORM POST
 
